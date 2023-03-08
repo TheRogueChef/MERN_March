43a0e616dc7de55d94e6321a43f51933bfdf688b
Erik Carlson <theroguechef@comcast.net>
2023-03-08 12:56:51 -0800

    'Prop it Up'

---
+++ week1/REACT/prop-it-up/prop-it-up/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Person from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Person 
+      lastName={'Doe'} 
+      firstName={'Jane'} 
+      age={'45'} 
+      hairColor={'Black'}  />
+      <Person 
+      lastName={'Smith'} 
+      firstName={'John'} 
+      age={'88'} 
+      hairColor={'Brown'}  />
+      <Person 
+      lastName={'Fillmore'} 
+      firstName={'Millard'} 
+      age={'50'} 
+      hairColor={'Brown'}  />
+      <Person 
+      lastName={'Smith'} 
+      firstName={'Maria'} 
+      age={'62'} 
+      hairColor={'Brown'}  />
+    </div>
+  );
+}
+
+export default App;
