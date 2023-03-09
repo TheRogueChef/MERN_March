9e8b200b053c58eeb7fee1119bb4c86d53763066
Erik Carlson <theroguechef@comcast.net>
2023-03-09 11:39:10 -0800

    'hook-form-project'

---
+++ week1/REACT/hook-form/hook-form/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from  'react';
+import './App.css';
+    
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [verifyPassword, setVerifyPassword] = useState("");  
+    
+    const createUser = (e) => {
+        // we must prevent the default refresh of the browser to keep our state from being reset
+        e.preventDefault();
+    
+        // create a javascript object to hold all of the values
+        const newUser = { 
+            firstname: firstName, 
+            lastname: lastName,
+            email: email, 
+            password: password,
+            verifyPassword: password,
+        };
+        console.log("Welcome", newUser);
+    };
+    
+    return(
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Verify Password: </label>
+                <input type="password" onChange={ (e) => setVerifyPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+            <h1>Your Form Data</h1>
+            <p>First Name: { firstName }</p>
+            <p>Last Name: { lastName }</p>
+            <p>Email: { email }</p>
+            <p>Password: { password }</p>
+            <p> Confirm Password: { verifyPassword }</p>
+        </form>
+    );
+    
+    
+};
+
+
+
+
+    <>
+
+    </>
+
+
+
+
+
+
+
+
+
+
+
+  export default UserForm;
